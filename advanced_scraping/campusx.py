6bf340ea333a882f64072c19a837a06eef24ee05
ali <ali_abde37@outlook.com>
2025-08-06 17:04:43 -0400

    basics

--- advanced_scraping/campusx.py
+++ advanced_scraping/campusx.py
@@ -1,8 +1,23 @@
 from selenium import webdriver
 import time
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
 
 s = Service("C:/Users/asus/OneDrive/Desktop/chromedriver.exe")
 
-webdriver.Chrome(service=s)
-time.sleep(10)
+driver = webdriver.Chrome(service=s)
+
+
+driver.get('http://google.com')
+time.sleep(2)
+
+#fetch the search input box using xpath
+user_input = driver.find_element(by=By.XPATH, value='/html/body/div[2]/div[4]/form/div[1]/div[1]/div[1]/div[1]/div[2]/textarea')
+user_input.send_keys('CampusX')
+time.sleep(2)
+
+user_input.send_keys(Keys.ENTER)
+
+time.sleep(20)
